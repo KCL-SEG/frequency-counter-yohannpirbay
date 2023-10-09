c7b8103af60310ced4bba3e3a05dbac5c816bdb3
Yohann Pirbay <pirbay.yohann@gmail.com>
2023-10-09 11:42:56 +0100

    Complete frequencies

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,10 @@
 
 def frequencies(items):
     frequencies = {}
+    for item in items:
+        if frequencies.get(str(item)) == None:
+            frequencies[str(item)] = 1
+        else:
+            frequencies[str(item)] = frequencies.get(str(item)) + 1
     # Your code goes here
     return frequencies
